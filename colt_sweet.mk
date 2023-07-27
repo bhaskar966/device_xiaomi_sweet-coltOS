0bbffc139ba7ca0b26e9f2bd1c8204fd709ba044
bhaskar966 <deybhaskar999@gmail.com>
2023-07-27 07:47:59 +0530

    sweet : ColtOS Bringup
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- colt_sweet.mk
+++ colt_sweet.mk
@@ -11,14 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Voltage OS  Stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 2160
-PRODUCT_NAME := voltage_sweet
+# Inherit common ColtOS Stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := colt_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-VOLTAGE_BUILD_TYPE := OFFICIAL
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# ColtOS Stuff
+COLT_BUILD_MAINTAINER := 𝙱𝚑𝚊𝚜𝚔𝚊𝚛
+COLT_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := false
